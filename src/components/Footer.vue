8c68297b5b386c6619a520d70ad3fa9b3d133268
Leo Huang <leo.huang@mykidx.com>
2019-08-20 17:07:54 +0800

    update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="footer">
-        Powered by <a href="https://help.github.com/en/articles/about-issues" target="_blank"> Github Issues </a> & <a href="https://cn.vuejs.org/index.html"> Vue </a>
+        Powered by <a href="https://help.github.com/en/articles/about-issues" target="_blank"> Github Issues </a> & <a href="https://cn.vuejs.org/index.html" target="_blank"> Vue </a>
     </div>
 </template>
 
